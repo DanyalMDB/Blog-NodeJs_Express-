f69d391c1f6284504995f760d3edff838a3d3d60
m772823-b1995 <muhammad.butt@stud.th-deg.de>
2021-08-29 04:35:15 +0200

    Installing EdgeJs Template Engine and Integrating pages

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
+//installed packed nodemon so server automatically restart after changes
+
+
+const path = require('path')
+
+const { config, engine } = require('express-edge');
+
 const express = require('express')
 
 
 //creating app
 const app = new express()
 
+app.use(express.static('public')) //public directory
+
+app.use(engine)
+
+app.set('views',`${__dirname}/views`)
+
+
+
+//setting up pages
+app.get('/',(req,res) =>{
+    res.render('index')
+})
+ 
+app.get('/contact',(req,res) =>{
+    res.render('contact')
+})
+app.get('/about',(req,res) =>{
+    res.render('about')
+})
+app.get('/post',(req,res) =>{
+    res.render('post')
+})
+
+
 
 //server
-
 app.listen(4200,()=>{
     console.log("App listening")
 })
+
+
