9c06eaa86159ffaf9c06705b01b6278de7e26f61
m772823-b1995 <muhammad.butt@stud.th-deg.de>
2021-08-31 04:46:45 +0200

    Facing problem getting data from database. To solve the issue Degraded mongoos.

--- test.js
+++ test.js
@@ -9,6 +9,39 @@ const Post = require('./database/models/Post')
 mongoose.connect('mongodb://localhost/nodeJs-Test-Blog')
 
 
+//extracting all data
+//Post is ModelFIle
+/*
+Post.find({}, function (err, docs) {
+    if (err){
+        console.log("Error function call : ", err);
+    }
+    else{
+        console.log("First function call : ", docs);
+    }
+});
+
+*/
+
+Post.findById("612be4226211c9b3b53800fb", function (err, post) {
+    if (err){
+        console.log(err);
+    }
+    else{
+        console.log("Result : ", post);
+    }
+});
+
+
+/*
+//extracting specfic data
+Post.find({
+    title: 'My first Blog Post'
+})
+
+*/
+
+/*
 //sync operation
 Post.create({
     id: 1,
@@ -18,3 +51,5 @@ Post.create({
 },(error, post)=>{
     console.log(error, post)
 })
+
+*/
