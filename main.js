2645c3d226dc4953f9b94589b88c72a27ff0d88f
m772823-b1995 <muhammad.butt@stud.th-deg.de>
2021-09-04 04:16:35 +0200

    Creating Custom Middleware and validate the post form

--- main.js
+++ main.js
@@ -24,6 +24,23 @@ const app = new express()
 //conecting
 mongoose.connect('mongodb://localhost/nodeJS-blog')
 
+//app.use() acts as a middleware in express apps.
+// you actually can use app.use() without specifying the request URL. 
+//In such a case what it does is, it gets executed every time no matter what URL's been hit.
+
+//creating Custom Middleware
+
+const validateCreatePostMiddleware = (req,res,next)=> {
+    
+    if(req.files == null || req.body.username === null ||
+         req.body.title === null || req.body.subtitle === null || 
+         req.body.content === null ){
+             
+        return res.redirect('/posts/new')
+    }
+    
+    next()
+}
 
 app.use(fileUpload())
 
@@ -36,8 +53,15 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
 
+
+app.use('/posts/store',validateCreatePostMiddleware)
+
+//The app.set() function is used to assigns the setting name to value. 
+//You may store any value that you want, but certain names can be used to configure the behavior of the server.
+
 app.set('views',`${__dirname}/views`)
 
+//app.get(): This function tells the server what to do when get requests at a given route.
 
 //setting up pages
 app.get('/', async (req,res) =>{
